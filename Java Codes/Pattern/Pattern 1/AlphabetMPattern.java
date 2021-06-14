3f9d17721bbb32a364ce9626629dc2cd22366630
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-14 22:57:41 +0530

    Pattern1 folder added

---
+++ Java Codes/Pattern/Pattern 1/AlphabetMPattern.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class AlphabetMPattern
+{
+	public static void main(String args[])
+	{
+		for(int i = 7;i>=1;i--)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		for(int i = 1;i<=6;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		for(int i = 6;i>=1;i--)
+		{
+			for(int j=i;j>=0;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		for(int i = 1;i<=6;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}		
+	}
+}
